7953021ce91c0e1a4cbc3d1ed62f181e64e75a2e
Smith23 <tenikaler.smith@utp.ac.pa>
2019-12-12 17:28:23 -0500

    se creo migración en ER

--- database/migrations/2019_07_13_040831_crear_tabla_permiso.php
+++ database/migrations/2019_07_13_040831_crear_tabla_permiso.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrearTablaPermiso extends Migration
+class  CrearTablaPermiso extends Migration
 {
     /**
      * Run the migrations.
