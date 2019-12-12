dc47a8f7da62fde724e2994313866eccc29ab1f4
Smith23 <tenikaler.smith@utp.ac.pa>
2019-12-12 14:23:31 -0500

    Master: actualizando laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -81,17 +81,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Teñikaler Smith
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Home</a>
+                    <a href="#">Blog</a>
+                    <a href="#">Portafolio</a>
+                    <a href="#">Galeria</a>
+                    <a href="#">Contacto</a>
+                    <a href="https://github.com/ArciSmith">GitHub - Teñikaler Smith</a>
                 </div>
             </div>
         </div>
